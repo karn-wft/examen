d0f6fd2b1eef4fa4f3f62e43050d3787b1fe9966
karn-wft <82561648+karn-wft@users.noreply.github.com>
2021-05-12 21:18:19 -0500

    Add files via upload

---
+++ Ex1.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main ()
+{
+    /*KAREN ITZEL GARDUÑO MARITANO 4IV17*/
+
+    int primer, segundo, multiplicar, division;
+        printf ("\n\n Porgrama 3");
+        printf("\n\n Saca el promedio de valores reales");
+        printf("\n\n Dame el primer valor real");
+        scanf ("%f",&primer);
+        printf("\n\n Dame el segundo valor real");
+        scanf ("% f", &Sefundo);
+        multiplicar = primer * segundo 
+        division = primer / segundo
+        printf ("\n El producto es %f",multiplicar);
+        printf ("\n El cociente es %f", division);
+        return 0;
+
+}
+    
+
